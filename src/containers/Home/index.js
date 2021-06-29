f486beea71552399ba6f4ea611e50923b084bf5e
anhtuannguyen <tuananh871995@gmail.com>
2021-06-30 01:10:22 +0700

    Create example

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -7,30 +7,25 @@ import './styles.css';
 
 const Home = () => {
 
-  const createTodo = () => {
-    const todoRef = firebase.database().ref('test');
-    const todo = {
-      title: 'test',
-      complete: false,
-      array: ['anh', 'tuan'],
-      array2: [1,2,3,4],
-      obj: {
-        test: 'hello',
-        anh: [
-          { x: 1},
-          {y: 2},
-        ]
-      }
-    };
-    console.log('todo ==>', todo);
-    todoRef.push(todo)
+  const exampleFunction = () => {
+    // const betRecordsRef = firebase.database().ref('BetRecords');
+
+    // const betRecordsRef = firebase.database().ref('BetRecords').child('id');
+    // betRecordsRef.remove();
+    // betRecordsRef.update({ data: '' });
+
+    // betRecordsRef.on('value', snapshot => {
+    //   const records = snapshot.val();
+    //   console.log('record ==>', records);
+    // });
+
+    // betRecordsRef.push(data)
   };
 
   return (
     <div className="app">
       <MenuBar/>
       <img src={banner} className="app-banner" alt="banner" />
-      <div onClick={createTodo}>click me</div>
       <Schedule/>
     </div>
   );
