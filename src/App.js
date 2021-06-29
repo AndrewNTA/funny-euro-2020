7a1a58cee20d66a283db2b0f2ee8f81383b9c410
anhtuannguyen <tuananh871995@gmail.com>
2021-06-29 11:15:19 +0700

    Add google login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import banner from './statistic/images/banner.jpeg';
 import logo from './statistic/images/logo-small.jpeg';
+import GoogleLogin from 'react-google-login';
 import './App.css';
 
 function App() {
+
+  const responseGoogleLogin = response => {
+    console.log('data ===>', response);
+  };
+
   return (
     <div className="app">
       <div className="menu-bar">
@@ -16,6 +22,13 @@ function App() {
         </div>
       </div>
       <img src={banner} className="app-banner" alt="banner" />
+      <GoogleLogin
+        clientId="442973200160-si3h4jb3e4bkbrt0gulqp71qdgi3bva8.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogleLogin}
+        onFailure={responseGoogleLogin}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
