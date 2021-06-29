b2fa388dbed7146fd71ef7f6ab2868ab30953eeb
anhtuannguyen <tuananh871995@gmail.com>
2021-06-29 12:54:28 +0700

    Handle save user login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import banner from './statistic/images/banner.jpeg';
 import logo from './statistic/images/logo-small.jpeg';
 import { useGoogleLogin } from 'react-google-login';
 import { gapi } from 'gapi-script';
+import {
+  CLIENT_ID,
+  ZALORA_EURO_PROFILE_ID,
+  ZALORA_EURO_PROFILE_NAME,
+  ZALORA_EURO_TOKEN
+} from './containers/Auth/constants';
+import cookie from './cookie';
 import './App.css';
 
-const logFn = res => { console.log('data ===>', res.tokenId, res.profileObj) };
-const errorFn = err => { console.log('error ===>', err) };
-const clientId = '442973200160-si3h4jb3e4bkbrt0gulqp71qdgi3bva8.apps.googleusercontent.com';
+const App = () => {
+  const currentUsername = cookie.get(ZALORA_EURO_PROFILE_NAME, { path: '/' }) || '';
+  const [username, setUsername] = useState(currentUsername);
+  const handleLoginSuccess = response => {
+    const tokenId = response.tokenId;
+    const profileId = response.googleId;
+    const profileName = response.profileObj && response.profileObj.name;
 
-function App() {
+    cookie.set(ZALORA_EURO_TOKEN, tokenId, { path: '/' });
+    cookie.set(ZALORA_EURO_PROFILE_ID, profileId, { path: '/' });
+    cookie.set(ZALORA_EURO_PROFILE_NAME, profileName, { path: '/' });
+
+    setUsername(profileName);
+  };
+
   const { signIn } = useGoogleLogin({
-    onSuccess: logFn,
-    clientId,
-    onFailure: errorFn,
+    onSuccess: handleLoginSuccess,
+    clientId: CLIENT_ID,
+    onFailure: () => {},
     cookiePolicy: 'single_host_origin',
   });
 
@@ -24,20 +41,30 @@ function App() {
         auth2.disconnect()
       )
     }
+    cookie.remove(ZALORA_EURO_TOKEN, { path: '/' });
+    cookie.remove(ZALORA_EURO_PROFILE_ID, { path: '/' });
+    cookie.remove(ZALORA_EURO_PROFILE_NAME, { path: '/' });
+
+    setUsername('')
   };
+
+  const tokenId = cookie.get(ZALORA_EURO_TOKEN, { path: '/' });
 
   return (
     <div className="app">
       <div className="menu-bar">
         <img src={logo} className="menu-logo" alt="banner" />
-        <div className="login-register-button" onClick={signIn}>
+        {tokenId ? <div className="menu-welcome">
+          <span className="welcome-text">Welcome</span>
+          <span className="username">{username}</span>
+        </div> : <div className="login-register-button" onClick={signIn}>
           Đăng ký / Đăng nhập
-        </div>
+        </div>}
       </div>
       <img src={banner} className="app-banner" alt="banner" />
       <div onClick={handleLogout}>Logout</div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/containers/Auth/constants.js
@@ -0,0 +1,11 @@
+const MODULE = 'AUTH';
+
+export const CLIENT_ID = '442973200160-si3h4jb3e4bkbrt0gulqp71qdgi3bva8.apps.googleusercontent.com';
+
+export const ZALORA_EURO_TOKEN = 'ZALORA_EURO_TOKEN';
+export const ZALORA_EURO_PROFILE_ID = 'ZALORA_EURO_PROFILE_ID';
+export const ZALORA_EURO_PROFILE_NAME = 'ZALORA_EURO_PROFILE_NAME';
+
+
+export const LOGIN_REQUEST = `${MODULE}/LOGIN_REQUEST`;
+export const LOGOUT_REQUEST = `${MODULE}/LOGOUT_REQUEST`;
