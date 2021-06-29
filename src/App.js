cb16105cea0be738eb59b157af7bc8308362f512
anhtuannguyen <tuananh871995@gmail.com>
2021-06-29 11:57:59 +0700

    Use hook and add cookie

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import banner from './statistic/images/banner.jpeg';
 import logo from './statistic/images/logo-small.jpeg';
-import GoogleLogin from 'react-google-login';
+import { useGoogleLogin, useGoogleLogout } from 'react-google-login';
 import './App.css';
+
+const logFn = res => { console.log('data ===>', res) };
+const errorFn = err => { console.log('error ===>', err) };
+const clientId = '442973200160-si3h4jb3e4bkbrt0gulqp71qdgi3bva8.apps.googleusercontent.com';
 
 function App() {
 
-  const responseGoogleLogin = response => {
-    console.log('data ===>', response);
-  };
+  const { signIn } = useGoogleLogin({
+    onSuccess: logFn,
+    clientId,
+    onFailure: errorFn,
+    cookiePolicy: 'single_host_origin',
+  });
+
+  const { signOut } = useGoogleLogout({
+    onLogoutSuccess: logFn,
+    clientId,
+    onFailure: errorFn,
+  });
 
   return (
     <div className="app">
       <div className="menu-bar">
-        <div className="menu-bar-header">
-          <img src={logo} className="menu-logo" alt="banner" />
-          <div className="menu-description">ZALORA bùng cháy cùng EURO</div>
-        </div>
-        <div className="login-register-button">
+        <img src={logo} className="menu-logo" alt="banner" />
+        <div className="login-register-button" onClick={signIn}>
           Đăng ký / Đăng nhập
         </div>
       </div>
       <img src={banner} className="app-banner" alt="banner" />
-      <GoogleLogin
-        clientId="442973200160-si3h4jb3e4bkbrt0gulqp71qdgi3bva8.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogleLogin}
-        onFailure={responseGoogleLogin}
-        cookiePolicy={'single_host_origin'}
-      />
+      <div onClick={signOut}>Logout</div>
     </div>
   );
 }
