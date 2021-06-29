6995d5a7f2c1b3f4c8bf2ce4d3ba7ace4aa41d7a
anhtuannguyen <tuananh871995@gmail.com>
2021-06-29 16:50:14 +0700

    Refactor

--- src/containers/Match/index.js
+++ src/containers/Match/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { data } from '../../core/data';
 import { useParams } from 'react-router-dom';
+import MenuBar from '../MenuBar';
 import './styles.css';
 
 const getDetailMatch = (matchId, dateId) => {
@@ -32,29 +33,32 @@ const Match = () => {
 
   const { date, round, detail } = matchDetail;
   return (
-    <div className="container-match-detail" id={matchId}>
-      <div className="top-match-detail">
-        <div className="tag-match-detail">{`${date}, ${detail.time}`}</div>
-        <div className="tag-match-detail">{round}</div>
-        <div className="match-detail-info">
-          <div className="team-match-detail">
-            <span className="team-name-match-detail align-right-match-detail">{detail.team1.name}</span>
-            <img alt="flag" src={detail.team1.flag} className="team-flag-match-detail"/>
-          </div>
-          <div className="middle-match-detail">
-            {detail.isFinished ? <div className="result-match-detail">
-              <span className="score-match-detail">{detail.team1.score}</span>
-              {detail.isPen && <span className="pen-match-detail">{`(${detail.team1.pen})`}</span>}
-              <span className="divider-match-detail">-</span>
-              <span className="score-match-detail">{detail.team2.score}</span>
-              {detail.isPen && <span className="pen-match-detail">{`(${detail.team2.pen})`}</span>}
-            </div> : <div className="info-match-detail">
-              {detail.info}
-            </div>}
-          </div>
-          <div className="team-match-detail">
-            <img alt="flag" src={detail.team2.flag} className="team-flag-match-detail"/>
-            <span className="team-name-match-detail">{detail.team2.name}</span>
+    <div>
+      <MenuBar/>
+      <div className="container-match-detail" id={matchId}>
+        <div className="top-match-detail">
+          <div className="tag-match-detail">{`${date}, ${detail.time}`}</div>
+          <div className="tag-match-detail">{round}</div>
+          <div className="match-detail-info">
+            <div className="team-match-detail">
+              <span className="team-name-match-detail align-right-match-detail">{detail.team1.name}</span>
+              <img alt="flag" src={detail.team1.flag} className="team-flag-match-detail"/>
+            </div>
+            <div className="middle-match-detail">
+              {detail.isFinished ? <div className="result-match-detail">
+                <span className="score-match-detail">{detail.team1.score}</span>
+                {detail.isPen && <span className="pen-match-detail">{`(${detail.team1.pen})`}</span>}
+                <span className="divider-match-detail">-</span>
+                <span className="score-match-detail">{detail.team2.score}</span>
+                {detail.isPen && <span className="pen-match-detail">{`(${detail.team2.pen})`}</span>}
+              </div> : <div className="info-match-detail">
+                {detail.info}
+              </div>}
+            </div>
+            <div className="team-match-detail">
+              <img alt="flag" src={detail.team2.flag} className="team-flag-match-detail"/>
+              <span className="team-name-match-detail">{detail.team2.name}</span>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/containers/MenuBar/index.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import logo from '../../static/images/logo-small.jpeg';
+import { useGoogleLogin } from 'react-google-login';
+import { useToasts } from 'react-toast-notifications';
+import {
+  CLIENT_ID,
+  ZALORA_EURO_PROFILE_EMAIL,
+  ZALORA_EURO_PROFILE_ID,
+  ZALORA_EURO_PROFILE_NAME,
+  ZALORA_EURO_TOKEN
+} from '../../core/constants';
+import cookie from '../../cookie';
+import './styles.css';
+
+const MenuBar = () => {
+  const history = useHistory();
+  const { addToast } = useToasts();
+  const currentUsername = cookie.get(ZALORA_EURO_PROFILE_NAME, { path: '/' }) || '';
+  const [username, setUsername] = useState(currentUsername);
+  const handleLoginSuccess = response => {
+    const tokenId = response.tokenId;
+    const profileId = response.googleId;
+    const profileName = response.profileObj && response.profileObj.name;
+    const profileEmail = response.profileObj && response.profileObj.email;
+
+    cookie.set(ZALORA_EURO_TOKEN, tokenId, { path: '/' });
+    cookie.set(ZALORA_EURO_PROFILE_ID, profileId, { path: '/' });
+    cookie.set(ZALORA_EURO_PROFILE_NAME, profileName, { path: '/' });
+    cookie.set(ZALORA_EURO_PROFILE_EMAIL, profileEmail, { path: '/' });
+
+    showToastSuccess();
+    setUsername(profileName);
+  };
+
+  const showToastSuccess = () => {
+    addToast('Đăng nhập thành công', {
+      appearance: 'success',
+      autoDismissTimeout: 3000,
+      autoDismiss: true,
+    });
+  };
+
+  const showToastFailed = () => {
+    addToast('Đăng nhập thất bại! Vui lòng thử lại', {
+      appearance: 'error',
+      autoDismissTimeout: 3000,
+      autoDismiss: true,
+    });
+  };
+
+  const { signIn } = useGoogleLogin({
+    onSuccess: handleLoginSuccess,
+    clientId: CLIENT_ID,
+    onFailure: () => {showToastFailed()},
+    cookiePolicy: 'single_host_origin',
+  });
+
+  const goToProfile = () => {
+    history.push('/profile');
+  };
+
+  const goHome = () => {
+    history.push('/');
+  };
+
+  const tokenId = cookie.get(ZALORA_EURO_TOKEN, { path: '/' });
+
+  return (
+    <div className="menu-bar">
+      <img src={logo} className="menu-logo" alt="logo" onClick={goHome}/>
+      {tokenId ? <div className="menu-welcome">
+        <span className="welcome-text">Welcome</span>
+        <span className="username" onClick={goToProfile}>{username}</span>
+      </div> : <div className="login-register-button" onClick={signIn}>
+        Đăng ký / Đăng nhập
+      </div>}
+    </div>
+  );
+};
+
+export default MenuBar;
