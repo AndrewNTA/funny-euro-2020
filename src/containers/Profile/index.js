2c9181f8c6ac51c1eae80668850d018399693acf
anhtuannguyen <tuananh871995@gmail.com>
2021-06-29 13:46:42 +0700

    Implement profile page

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import banner from '../../statistic/images/banner.jpeg';
+import { useHistory } from 'react-router-dom';
 import { gapi } from 'gapi-script';
 import {
   ZALORA_EURO_PROFILE_EMAIL,
@@ -11,6 +12,7 @@ import cookie from '../../cookie';
 import './styles.css';
 
 const Profile = () => {
+  const history = useHistory();
   const profileUsername = cookie.get(ZALORA_EURO_PROFILE_NAME, { path: '/' }) || '';
   const profileEmail = cookie.get(ZALORA_EURO_PROFILE_EMAIL, { path: '/' }) || '';
 
@@ -27,21 +29,33 @@ const Profile = () => {
     cookie.remove(ZALORA_EURO_PROFILE_EMAIL, { path: '/' });
   };
 
+  const goHome = () => {
+    history.push('/');
+  };
 
   return (
     <div className="container">
       <img src={banner} className="app-banner" alt="banner" />
       <div className="wrapper">
+        <div className="title">
+          Thông tin cá nhân
+        </div>
         <div className="row">
           <span className="label">Tên:</span>
           <span className="info">{profileUsername}</span>
         </div>
         <div className="row">
-          <span className="label">Tên:</span>
+          <span className="label">Email:</span>
           <span className="info">{profileEmail}</span>
         </div>
+        <div className="control-group">
+          <div className="home-btn" onClick={goHome}>Trang chủ</div>
+          <div className="logout-btn" onClick={handleLogout}>Đăng xuất</div>
+        </div>
+        <div className="title space">
+          Lịch sử dự đoán
+        </div>
       </div>
-      <div onClick={handleLogout}>Đăng xuất</div>
     </div>
   );
 };
